4ac4fd81b807735b48ad5a2831073e414d4bf488
Muffin-Man2 <85885968+Muffin-Man2@users.noreply.github.com>
2022-01-03 11:45:29 +0800

    day 1 completed

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,34 @@
+use std::fs;
+
+const FILENAME : &str = "input.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string(FILENAME)
+        .expect("Something went wrong reading the file");
+    let split = contents.split("\n");
+    // had to remove the last line for the parse to work correctly
+    let mut data = split.map(|x| x.parse::<i32>().unwrap());
+    let vector : Vec<_> = data.clone().collect();
+
+    let mut previous_value = data.next().unwrap();
+    let mut total = 0;
+    for value in data {
+        if value > previous_value {
+            total += 1;
+        }
+        previous_value = value;
+    }
+    println!("The total measurements that increased were {}.", total);
+
+    let mut prev_sum : i32 = vector[0] + vector[1] + vector[2];
+    let mut sum : i32;
+    total = 0;
+    for index in 1..(vector.len()-2) {
+        sum = vector[index] + vector[index+1] + vector[index+2];
+        if sum > prev_sum {
+            total += 1;
+        }
+        prev_sum = sum;
+    }
+    println!("The total amount of sum increases were {}.", total);
 }
